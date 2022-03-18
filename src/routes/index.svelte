f63d482a5f12206496fecd3b21fde6c95cf36d2c
Alim Giray Aytar <alimgiray@gmail.com>
2022-03-18 22:40:36 +0300

    create input & title

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -1,2 +1,15 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<script>
+</script>
+
+<main class="p-6">
+	<h1 class="text-2xl font-bold text-center">My TODOs</h1>
+	<form class="flex justify-center my-4">
+		<input
+			type="text"
+			name="todo"
+			placeholder="learn svelte..."
+			class="p-2 text-md w-full md:w-1/3 border border-gray-300 outline-none focus:border-gray-900"
+		/>
+		<button class="border py-2 px-4 border-gray-300 outline-none focus:border-gray-900">+</button>
+	</form>
+</main>
